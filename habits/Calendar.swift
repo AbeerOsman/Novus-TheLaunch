0ea9b176dbcb3f771e202d7efec5945911336f06
AbeerOsman <abeerjilani90@gmail.com>
2025-10-03 20:37:53 +0300

    Habites-code

---
+++ habits/Calendar.swift
@@ -0,0 +1,18 @@
+//
+//  Calendar.swift
+//  habits
+//
+//  Created by Abeer Jeilani Osman  on 11/04/1447 AH.
+//
+
+import SwiftUI
+
+struct Calendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Calendar()
+}
---
+++ habits/HabitsShuffle.swift
@@ -0,0 +1,18 @@
+//
+//  HabitsShuffle.swift
+//  habits
+//
+//  Created by Abeer Jeilani Osman  on 09/04/1447 AH.
+//
+
+import SwiftUI
+
+struct HabitsShuffle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitsShuffle()
+}
